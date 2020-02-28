991ed8cd2771cb02137cf7780c83cd9b95c95c63
Jinghao Xu <jix188@ucsd.edu>
2020-02-27 23:37:00 -0800

    completed A8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,7 @@
 function initMap() {
-	// add your code here
+    // add your code here
+    L.mapquest.key = 'bImtOatx5SGVkk0m2K3kCK6R8RxFfV40';
+
+    var map = L.mapquest.map('map', { center: [32.88, -117.236], layers: L.mapquest.tileLayer('map'), zoom: 12, zoomControl: false });
+    L.marker([32.88, -117.236]).addTo(map);
 }
